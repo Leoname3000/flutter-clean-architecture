1c5a50e920e4daf20df85ca17dd7b40fbe1e1f56
Leoname3000 <sulin.leonid@gmail.com>
2022-02-04 15:04:11 +0300

    Split into folders

---
+++ lib/domain/model/day.dart
@@ -0,0 +1,13 @@
+class Day {
+  final DateTime sunrise;
+  final DateTime sunset;
+  final DateTime solarNoon;
+  final int dayLight;
+
+  Day({
+    required this.sunrise,
+    required this.sunset,
+    required this.solarNoon,
+    required this.dayLight,
+  });
+}
---
+++ lib/internal/application.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture/presentation/home.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
